a8a9bb60b21e5856218edbbfb5cebfa2f55a443c
Joey Clark <62483206+joeyksclark@users.noreply.github.com>
2021-09-11 16:45:30 -0700

    Update my author username in always_paste_password.js

--- Tampermonkey/always_paste_password.js
+++ Tampermonkey/always_paste_password.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  onpaste="return false;" doesn't exist in a perfect world...
-// @author       jkclark
+// @author       joeyksclark
 // @match        *://*/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
